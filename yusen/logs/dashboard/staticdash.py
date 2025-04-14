c6e7b72e9a6bc145f232a250b2800aea1fc80f42
Sooryan98 <sooryan1998@gmail.com>
2025-04-14 01:31:07 -0400

    update

--- yusen/logs/dashboard/staticdash.py
+++ yusen/logs/dashboard/staticdash.py
@@ -5,8 +5,8 @@ import re
 import os
 from collections import defaultdict
 from datetime import datetime
-DESTRO_PATH = "/home/soorya/destro_core/Destro_Yusen/cross-docking/logs/yusen_2025-04-10.log"
-FMS_PATH = "/home/soorya/destro_python/yunsen/logs/FMS_2025-04-10.log"
+DESTRO_PATH = "yusen/logs/inputlog/yusen_2025-04-10.log"
+FMS_PATH = "yusen/logs/inputlog/FMS_2025-04-10.log"
 
 st.set_page_config(page_title="destro", layout="wide")
 
@@ -118,7 +118,7 @@ robot_dist_df = pd.DataFrame(list(robot_fms_data.items()), columns=["Robot", "Di
 progress_df=pd.DataFrame(list(progress.items()), columns=["Hour", "Cases"])
 robot_uph_df=pd.DataFrame(list(robot_total_cases.items()), columns=["Robot", "Total Cases"])
 # ---------------- Display Dashboard ----------------
-st.image("/home/soorya/destro_dashboard/destro_dashboard/yusen/logs/destro_logo.png", width=400)
+st.image("yusen/logs/destro_logo.png", width=400)
 st.metric(label="Total Cases Picked", value=log_data['total_cases'])
 
 # chart_cases = alt.Chart(robot_cases_df).mark_bar().encode(
